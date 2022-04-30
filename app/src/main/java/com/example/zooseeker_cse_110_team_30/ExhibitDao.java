e362f7ecc71928bf5d0bec837bdfc27fd7d234ff
BrandonZWang <42730012+BrandonZWang@users.noreply.github.com>
2022-04-30 03:17:21 -0700

    Finished comments for ExhibitDao.java

--- app/src/main/java/com/example/zooseeker_cse_110_team_30/ExhibitDao.java
+++ app/src/main/java/com/example/zooseeker_cse_110_team_30/ExhibitDao.java
@@ -26,6 +26,7 @@ public interface ExhibitDao {
      * Accessor for retrieving all Exhibits matching a name or tag substring.
      * @param query The String to search for.
      * @return All Exhibits where the query is a substring of name or tags, ordered alphabetically.
+     * @see "https://www.sqlite.org/lang_expr.html"
      */
     @Query("SELECT * FROM exhibits WHERE kind='exhibit' " + //only return exhibits, no gates etc
             " AND (name LIKE ('%' || :query || '%')" + //name pattern match (|| = concatenate)
@@ -33,15 +34,36 @@ public interface ExhibitDao {
             " ORDER BY name ASC") //order alphabetically
     List<Exhibit> getSearch(String query);
 
-    @Query("SELECT * FROM exhibits WHERE kind='exhibit'")
+    //TODO figure out if we can combine getAll() and getAllLive()
+    /**
+     * Accessor for retrieving all Exhibits unconditionally.
+     * @return A list of every Exhibit in this DAO.
+     */
+    @Query("SELECT * FROM exhibits")
     List<Exhibit> getAll();
 
+    /**
+     * Accessor for live exhibits for use in LiveData.
+     * @return all exhibits currently in the DAO, sorted alphabetically by name.
+     */
     @Query("SELECT * FROM exhibits WHERE kind='exhibit' ORDER BY name ASC")
     LiveData<List<Exhibit>> getAllLive();
 
+    /**
+     * Inserts all Exhibits from a list into the DAO.
+     * @param exhibits The List of Exhibits to add to the DAO.
+     * @return A List of the IDs of every Exhibit inserted into the DAO.
+     * @see "https://androidx.de/androidx/room/EntityInsertionAdapter.html#insertAndReturnIdsList(java.util.Collection%3C?%20extends%20T%3E)"
+     */
     @Insert
-    List<Long> insertAll(List<Exhibit> exhibit);
+    List<Long> insertAll(List<Exhibit> exhibits);
 
+    /**
+     * Updater for Exhibits in this DAO.
+     * @param exhibit The Exhibit to update, containing updated
+     * @return The number of rows in the DAO affected by this update operation.
+     * @see "https://"androidx.de/androidx/room/EntityDeletionOrUpdateAdapter.html#handle(T)"
+     */
     @Update
     int update(Exhibit exhibit);
 }
