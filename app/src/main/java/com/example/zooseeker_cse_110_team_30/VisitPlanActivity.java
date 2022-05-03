46085e101ff828f889c4f07d30e6c198259e5e67
BrandonZWang <42730012+BrandonZWang@users.noreply.github.com>
2022-05-03 01:53:00 -0700

    Added new visit plan activity

---
+++ app/src/main/java/com/example/zooseeker_cse_110_team_30/VisitPlanActivity.java
@@ -0,0 +1,14 @@
+package com.example.zooseeker_cse_110_team_30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_plan);
+    }
+}
