164651e939f353b90e0e9a3ac6439fe1f78fe500
BrandonZWang <42730012+BrandonZWang@users.noreply.github.com>
2022-05-04 03:24:00 -0700

    Added plan button to MainActivity (#48)

--- app/src/main/java/com/example/zooseeker_cse_110_team_30/VisitPlanActivity.java
+++ app/src/main/java/com/example/zooseeker_cse_110_team_30/VisitPlanActivity.java
@@ -5,10 +5,11 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 import java.util.List;
 
@@ -17,11 +18,12 @@ import java.util.List;
  * @see "https://developer.android.com/reference/android/app/Activity.html"
  */
 public class VisitPlanActivity extends AppCompatActivity {
-    public RecyclerView recyclerView; //for search results display
+    public RecyclerView recyclerView; //for plan display
     public ExhibitViewModel viewModel; //manages UI data + handlers
-    private ImageButton searchButton; //search button for search bar
-    private EditText searchBar; //search bar for exhibits
+    private Button directionsButton; //directions button for next Activity
     private ExhibitAdapter adapter; //adapts DAO/lists of exhibits to UI
+
+    private List<IdentifiedWeightedEdge> edgeList; //list of edges in the visit plan, ordered.
 
     /**
      * Function that runs when this Activity is created. Set up most classes.
@@ -31,7 +33,10 @@ public class VisitPlanActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //update which layout is displaying
+        setContentView(R.layout.activity_visit_plan); //update which layout is displaying
+
+        Bundle extras = getIntent().getExtras();
+        this.edgeList = (List)extras.get("visit_list");
 
         viewModel = new ViewModelProvider(this)
                 .get(ExhibitViewModel.class); //get ExhibitViewModel from the provider
@@ -39,45 +44,26 @@ public class VisitPlanActivity extends AppCompatActivity {
         //create ExhibitAdapter and set it up
         adapter = new ExhibitAdapter(); //create adapter
         adapter.setHasStableIds(true);
-        adapter.setOnCheckBoxClickedHandler(viewModel::toggleSelected); //exhibit selection handler
         //get and start observing LiveData Exhibits. When change detected, call setExhibits.
         viewModel.getExhibits().observe(this, adapter::setExhibits);
 
         //get RecyclerView from layout and set it up
-        this.recyclerView = findViewById(R.id.zoo_exhibits);
+        this.recyclerView = findViewById(R.id.visit_exhibits);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
-        this.searchBar = this.findViewById(R.id.search_bar); //get search bar from layout
-
-        //set up search button click listener/handler
-        this.searchButton = this.findViewById(R.id.search_button); //get search button from layout
-        searchButton.setOnClickListener(this::onSearchButtonClicked);
-        //adapter.setExhibits(Exhibit.loadJSON(this, "sample_node_info.json"));
+        //set up directions button click listener/handler
+        this.directionsButton = this.findViewById(R.id.directions_button); //get button from layout
+        directionsButton.setOnClickListener(this::onDirectionsButtonClicked);
     }
 
     /**
-     * Event handler for clicking the search button.
-     * @param view The View which contains the search button and search bar.
+     * Event handler for clicking the directions button button.
+     * @param view The View which contains the directions button.
      */
-    public void onSearchButtonClicked(View view) {
-        String text = this.searchBar.getText().toString(); //get search bar text
-
-        List<Exhibit> searchResults;
-        if(text.equals("")) { //if the search box is empty, display all exhibits
-            searchResults = viewModel.getAllExhibits();
-        }
-        else { //search bar contains some text
-            //remove commas from query to prevent unexpected substring behavior
-            //ex. "r,r" returns Alligators because the tags: "alligator,reptile"
-            //TODO try typing out "alligator" with lots of commas everywhere
-            text = text.replace(",", "");
-            searchResults = viewModel.query(text); //get search results from DAO
-        }
-        adapter.setExhibits(searchResults); //update list of displayed exhibits
-
-        /* debug messages
-        System.out.println("Search text: \"" + text + "\"");
-        System.out.println(searchResults.toString()); */
+    public void onDirectionsButtonClicked(View view) {
+        //Intent directionsIntent = new Intent(this, DirectionsActivity.class);
+        //directionsIntent.putExtra("visit_list", this.edgeList);
+        //startActivity(directionsIntent);
     }
 }
